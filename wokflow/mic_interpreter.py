3d33b3cd8b3cdc687e20f92d29720c5fbf9f85e3
Ferdinand Rath <ferdinand.rath@web.de>
2025-02-19 09:56:20 +0100

    Updated Parser and Interpreter for new format translations.csv file for manual translations names.csv for same names everywhere

--- wokflow/mic_interpreter.py
+++ wokflow/mic_interpreter.py
@@ -5,11 +5,12 @@
 from enum import Enum
 import pandas as pd
 from fuzzywuzzy import fuzz
+import os
 
 OUTPUT_PATH = "output/mic_interpretation.csv"
 INPUT_EUCAST_PATH = "resources/MIC_Enterobacterales_v15_0.json"
-INPUT_VITEK_PATH = "output/vitek_parsed.csv"
-
+INPUT_VITEK_PATH = "output/vitek_parsed/"
+NAMES_PATH = "resources/names.csv"
 
 class EucastInterpretation(Enum):
     S = 1
@@ -19,6 +20,7 @@ class EucastInterpretation(Enum):
 
 def get_most_similar_name(input_df, target_name, cut_off):
     input_df = input_df.copy()
+    input_df['Name'] = input_df['Name'].str.split("(", n=1).str[0].str.strip()
     input_df["similarity_score"] = input_df["Name"].apply(
         lambda name: fuzz.ratio(target_name, name)
     )
@@ -30,60 +32,87 @@ def get_most_similar_name(input_df, target_name, cut_off):
 
     return best_match
 
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 def get_mic_interpretation(columns_vitek, rows_eucast, antibiotic_name_vitek, df):
     index = 0
     for data in columns_vitek:
         interpretation = ""
-        data = data.replace(">", "").replace("=", "").replace(",", ".")
-        if "<" in data:
-            interpretation = EucastInterpretation(1).name
-        elif float(data) <= float(rows_eucast["S <="].iloc[0]):
-            interpretation = EucastInterpretation(1).name
-        elif float(data) <= float(rows_eucast["R >"].iloc[0]):
-            interpretation = EucastInterpretation(2).name
+        if isinstance(data, float):
+            interpretation = "NA"
+            print(f"Data not usable: {data}")
+        elif is_float(rows_eucast["S <="].iloc[0]) and is_float(rows_eucast["R >"].iloc[0]):
+            data = data.replace(">", "").replace("=", "").replace(",", ".")
+            if "<" in data:
+                interpretation = EucastInterpretation(1).name
+            elif float(data) <= float(rows_eucast["S <="].iloc[0]):
+                interpretation = EucastInterpretation(1).name
+            elif float(data) <= float(rows_eucast["R >"].iloc[0]):
+                interpretation = EucastInterpretation(2).name
+            else:
+                interpretation = EucastInterpretation(3).name
         else:
-            interpretation = EucastInterpretation(3).name
+            interpretation = "NA"
+            print("Eucast not usable")
 
         df.at[index, antibiotic_name_vitek] = interpretation
         index += 1
     return df
 
+def interpret_vitek(input_vitek, input_eucast):
+    output_df = input_vitek[["LABORNR", "Organism_Code"]].copy() # Karte rausgenommen & Sample_id_ifh zu LABORNR geändert
+    # Clean VITEK data
+    input_vitek.columns = input_vitek.columns.str.strip()
+    input_vitek = input_vitek.map(lambda x: x.strip() if isinstance(x, str) else x)
+    #input_vitek = input_vitek.replace("NA", None)
+    #input_vitek = input_vitek.dropna(axis=1, how="all")
 
-input_vitek = pd.read_csv(INPUT_VITEK_PATH)
-input_eucast = pd.read_json(INPUT_EUCAST_PATH)
+    # Process each antibiotic column
+    for column_vitek in input_vitek.columns[2:]:
+        # Adapt VITEK name to EUCAST
+        #column_name_vitek = column_vitek.split("-", 1)[1]
 
-output_df = input_vitek[["Sample_ID_IfH", "Organism_Code", "Card_Name"]].copy()
+        column_name_vitek = column_vitek.split("(", 1)[0].replace("/", "-").replace("+", "-").replace(" ", "")
+        
+        matching_rows_eucast = input_eucast.loc[
+            input_eucast["Name"].str.contains(column_name_vitek, case=False, na=False)
+        ]
+        
+        if not matching_rows_eucast.empty:
+            matching_rows_eucast = get_most_similar_name(
+                matching_rows_eucast, column_name_vitek, 70
+            )
+        else:
+            print(f"No EUCAST Match: {column_name_vitek}")
+        if matching_rows_eucast.empty:
+            continue
 
-# Clean VITEK data
-input_vitek.columns = input_vitek.columns.str.strip()
-input_vitek = input_vitek.map(lambda x: x.strip() if isinstance(x, str) else x)
-input_vitek = input_vitek.replace("NA", None)
-input_vitek = input_vitek.dropna(axis=1, how="all")
-
-# Process each antibiotic column
-for column_vitek in input_vitek.columns[3:]:
-    # Adapt VITEK name to EUCAST
-    column_name_vitek = column_vitek.split("-", 1)[1]
-    column_name_vitek = column_name_vitek.replace("/", "-")
-
-    matching_rows_eucast = input_eucast.loc[
-        input_eucast["Name"].str.contains(column_name_vitek, case=False, na=False)
-    ]
-
-    if not matching_rows_eucast.empty:
-        matching_rows_eucast = get_most_similar_name(
-            matching_rows_eucast, column_name_vitek, 70
+        column_data_vitek = input_vitek[column_vitek]
+        output_df = get_mic_interpretation(
+            column_data_vitek, matching_rows_eucast, column_name_vitek, output_df
         )
-    else:
-        print(f"No EUCAST Match: {column_vitek}")
-    if matching_rows_eucast.empty:
-        continue
+    
+    return output_df
 
-    column_data_vitek = input_vitek[column_vitek]
-    output_df = get_mic_interpretation(
-        column_data_vitek, matching_rows_eucast, column_name_vitek, output_df
-    )
+# Folder durchgehen & alles interpretieren
 
-output_df.to_csv(OUTPUT_PATH, index=False)
-print(f"Cleaned file saved to: {OUTPUT_PATH}")
+names_df = pd.read_csv(NAMES_PATH)
+
+for vitek_file_name in os.listdir(INPUT_VITEK_PATH):
+    if vitek_file_name.endswith('.csv'):
+        input_vitek = pd.read_csv(INPUT_VITEK_PATH + vitek_file_name)
+        eucast_file_name = names_df.loc[names_df['Code'] == input_vitek['Organism_Code'].iloc[0], 'Eucast_File_Name'].values[0]
+        input_eucast = pd.read_json(f"resources/{eucast_file_name}")
+        print(vitek_file_name, eucast_file_name)
+        output_df = interpret_vitek(input_vitek, input_eucast)
+        # Alle leeren Spalten droppen
+        output_df = output_df.replace("NA", pd.NA)
+        output_df = output_df.dropna(axis=1, how='all')
+        output_df = output_df.fillna("NA")
+        output_df.to_csv(f"output/interpreted/{vitek_file_name}", index=False)
+        print(f"Cleaned file saved to: output/interpreted/{vitek_file_name}")
--- wokflow/vitek_parser_new.py
+++ wokflow/vitek_parser_new.py
@@ -1,33 +1,31 @@
 import pandas as pd
 from fuzzywuzzy import fuzz
 import re
+from pprint import pprint
+import os
 
-def clean_dataframe(input_df, bacteria):
-    bakterien = ""
-    # Filter Zeilen mit token_set_ratio damit nach stichwörtern gesucht wird
-    input_df = input_df[input_df['ERREGERLANG'].apply(lambda x: fuzz.token_set_ratio(str(x).lower(), bacteria.lower()) >= 52 if pd.notna(x) else False)]
-    # Leeres Dataframe nur mit spalte 'LABORNR' erzeugen
+def clean_dataframe(input_df, bacteria, code):
+    print(f"Keys used: {bacteria}")
+    input_df = input_df[input_df['ERREGERLANG'].isin(bacteria)]
     df = pd.DataFrame(columns=['LABORNR'])
-    # Alle Rows durchgehen
     for index, row in input_df.iterrows():
-        if not row['ERREGERLANG'] in bakterien:
-            bakterien += row['ERREGERLANG'] + ","
-        # Falls Labornr nicht vorhanden -> neue Zeile erstellen
         if not df['LABORNR'].isin([row['LABORNR']]).any():
             new_row = {col: (row['LABORNR'] if col == 'LABORNR' else 'NA') for col in df.columns}
             new_row_df = pd.DataFrame([new_row])
             df = pd.concat([df, new_row_df], ignore_index = True)
-        # Falls Antibiotika nicht vorhanden -> neue Spalte erstellen
         if not row['ANTIBIOTIKA'] in df.columns:
-            df[row['ANTIBIOTIKA']] = 'NA'
-        # Wert einsetzen
+            if not str(row['ANTIBIOTIKA']) == "nan":
+                df[row['ANTIBIOTIKA']] = 'NA'
         i = df.loc[df['LABORNR'] == row['LABORNR']].index[0]
-        df.at[i, row['ANTIBIOTIKA']] = (str(row['TESTUNG']) + str(row['MHK-VKZ']) + str(row['MHK-Wert'])).replace("nan", "")
-    print(bakterien)
+        #df.at[i, row['ANTIBIOTIKA']] = (str(row['TESTUNG']) + str(row['MHK-VKZ']) + str(row['MHK-Wert'])).replace("nan", "")
+        value = (str(row['MHK-VKZ']) + str(row['MHK-Wert'])).replace("nan", "")
+        if not value == "":
+            df.at[i, row['ANTIBIOTIKA']] = value
+    df.insert(loc=1, column='Organism_Code', value=code)
     return df
 
-# Allgemeine Ähnlichkeit der einzigartigen Bakterienbekennzeichnungen überprüfen
-def check_similarity(df, bacteria):
+# allgemeine Ähnlichkeit der einzigartigen Bakterienbekennzeichnungen überprüfen
+def check_similarity(df, bacteria): # nicht verwendet
     unique_bacteria = df['ERREGERLANG'].dropna().unique()
 
     print(f"🔍 Überprüfung der Ähnlichkeit mit: '{bacteria}'")
@@ -41,18 +39,86 @@ def clean_text(text):
         return ""
     return re.sub(r"[^a-zA-Z\s]", "", text).lower()
 
+def confirm(question):
+    while True:
+        answer = input(f"{question} (y/n): ").strip().lower()
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            print("Invalid Input")
+
+# einzigartige Bakterienbekennzeichnungen zu Kategorien hinzufügen
+def assign_unique(df, categories):
+    unique_bacteria = df['ERREGERLANG'].dropna().unique()
+    assignments = {c: [] for c in categories}
+
+    for b in unique_bacteria:
+        scores = []
+        for c in categories:
+            similarity = fuzz.token_set_ratio(clean_text(b), clean_text(c))
+            scores.append(similarity)
+        highest_index = scores.index(max(scores))
+        assignments[categories[highest_index]].append(b)
+
+    pprint(assignments)
+    if confirm("Continue with assignments?"):
+        return assignments
+    else:
+        return []
+
+# Alle Outputs zu einer Datei zusammenfassen
+def combine(input_folder):
+    combined_df = pd.DataFrame()
+
+    for file in os.listdir(input_folder):
+        if file.endswith(".csv"):
+            file_path = os.path.join(input_folder, file)
+            df = pd.read_csv(file_path)
+            combined_df = pd.concat([combined_df, df], ignore_index=True).fillna("NA")
+    #combined_df.fillna("NA").replace("", "NA")
+
+    return combined_df
+
+def translate(input_df, translations_df):
+    for old in translations_df['Old']:
+        input_df = input_df.rename(columns=lambda col: col.replace(old, translations_df.loc[translations_df['Old'] == old, 'New'].values[0]))
+    return input_df
+
+BACTERIA = ["Escherichia Coli", "Klebsiella Pneumoniae", "Acinetobacter Baumannii", "Pseudomonas Aeruginosa", "Enterobacter Cloacae"]
+CODES = ["ECO", "EKPN", "PTX", "PPA", "EECG"]
+
 # Paths
 INPUT_PATH = "resources/UKM_vitek_daten_2.csv"
-OUTPUT_PATH = "output/vitek_parsed_ecoli.csv"
+NAMES_PATH = "resources/names.csv"
+TRANSLATIONS_PATH = "resources/translations.csv"
+OUTPUT_FOLDER = "output/vitek_parsed/"
+OUTPUT_PATH = "output/vitek_parsed.csv"
 
 # Load
 input_df = pd.read_csv(INPUT_PATH, sep=",")
+names_df = pd.read_csv(NAMES_PATH, sep=",")
+translations_df = pd.read_csv(TRANSLATIONS_PATH, sep=",")
 
-check_similarity(input_df, "Escherichia coli")
+# Assign all Bacteria to categories
+assignments = assign_unique(input_df, names_df['Vitek_Name'])
 
-# Clean
-cleaned_df = clean_dataframe(input_df, "Escherichia coli")
+# Clean & Save
+if assignments:
+    for name in names_df['Vitek_Name']:
+        cleaned_df = clean_dataframe(input_df, assignments[name], names_df.loc[names_df['Vitek_Name'] == name, 'Code'].values[0])
+        cleaned_df = translate(cleaned_df, translations_df)
+        cleaned_df.to_csv(OUTPUT_FOLDER + f"{name.lower().replace(" ", "_")}.csv", index=False)
+        print(f"All {name} saved to {OUTPUT_FOLDER + f"{name.lower().replace(" ", "_")}.csv"}")
 
-# Save
-cleaned_df.to_csv(OUTPUT_PATH, index=False)
-print(f"Cleaned file saved to: {OUTPUT_PATH}")
+'''
+combined_df = combine(OUTPUT_FOLDER)
+combined_df.to_csv(OUTPUT_PATH, index=False)
+'''
+
+# Escherichia Coli (ECO) = Enterobacterales.json
+# Klebsiella Pneumoniae (EKPN) = Enterobacterales.json
+# Acinetobacter Baumannii (PTX) = Pseudomonas.json
+# Pseudomonas Aeruginosa (PPA) = Pseudomonas.json
+# Enterobacter Cloacae (EECG) = Enterobacterales.json
