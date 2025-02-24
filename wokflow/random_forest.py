516eab3b185a6f3170ca789fd76b3262b8c3067d
Julian-W98 <julianwellingwork@gmail.com>
2025-02-24 08:13:32 +0100

    Plot ROC

--- wokflow/random_forest.py
+++ wokflow/random_forest.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve, auc, RocCurveDisplay
+from sklearn.preprocessing import label_binarize
 import preprocessing
 
+# Load data
 data_loader = preprocessing.DataLoader()
 # TODO: Paths shall be set via terminal
 preprocessed_data = data_loader.get_preprocessed_data(
@@ -13,17 +17,36 @@ preprocessed_data = data_loader.get_preprocessed_data(
 X = preprocessed_data.merged_input[preprocessed_data.feature_cols]
 y = preprocessed_data.merged_input[preprocessed_data.target_cols]
 
+
+# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.4, random_state=42
 )
 
+# Train Random Forest Model
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(X_train, y_train)
 
+# Get probability predictions (needed for ROC)
+y_score = rf_model.predict_proba(X_test)
 y_pred = rf_model.predict(X_test)
+print(y_pred[:, 11])
+# TODO Bring back accuracy
 
-y_pred_df = pd.DataFrame(y_pred, columns=preprocessed_data.target_cols)
+# TODO Calculate for all antibiotics
+# TODO Calculate for R, I and S?
+y_score_roc = np.array(y_score[11])[:, 0]
+print(y_score_roc)
+y_test_roc = np.char.strip(np.array(y_test.iloc[:, 11].astype(str), dtype=str))
+print(y_test_roc)
+fpr, tpr, thresholds = roc_curve(y_test_roc, y_score_roc, pos_label="R")
+print(fpr)
+print(tpr)
+roc_auc = auc(fpr, tpr)
 
-for col in y_test.columns:
-    accuracy = accuracy_score(y_test[col], y_pred_df[col])
-    print(f"Accuracy for {col}: {accuracy}")
+print(roc_auc)
+display = RocCurveDisplay(
+    fpr=fpr, tpr=tpr, roc_auc=roc_auc, estimator_name="example estimator"
+)
+display.plot()
+plt.show()
