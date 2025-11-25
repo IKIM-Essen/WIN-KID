f4a562bc1995c4a3c4c95d5692ddedfe5a5564ed
MiriamBal <87427584+MiriamBalzer@users.noreply.github.com>
2025-11-25 16:30:26 +0100

    Refactor logging and comments in preprocessing.py

--- workflow/preprocessing.py
+++ workflow/preprocessing.py
@@ -426,7 +426,6 @@ class DataLoader:
             fasta_ids = self.merged_input[ID_COLUMN].unique().tolist()
 
             train_ids = random.sample(fasta_ids, min(TRAIN_SUBSET_SIZE, len(fasta_ids)))
-            #train_ids = fasta_ids
             logger.info(f"Train Word2Vec on all {len(train_ids)} FASTA files using streaming...")
             w2v_model = train_word2vec_model_streaming(train_ids, fasta_dir, min_count=2)
 
@@ -434,13 +433,13 @@ class DataLoader:
             logger.info("Generate aggregated k-mer embeddings...")
             embedding_df = encode_all_samples(fasta_ids, fasta_dir, w2v_model)
 
-            logger.info(f"🧬 Embedding DataFrame shape: {embedding_df.shape}")
-            logger.info(f"🧬 Embedding columns: {embedding_df.columns.tolist()[:5]}...")
+            logger.info(f" Embedding DataFrame shape: {embedding_df.shape}")
+            logger.info(f" Embedding columns: {embedding_df.columns.tolist()[:5]}...")
 
 
             logger.info(f"Add {embedding_df.shape[1]-1} k-mer embeddings to feature columns ...")
             self.merged_input = pd.merge(self.merged_input, embedding_df, on=ID_COLUMN, how="left")
-            # Entferne alle Zeilen, bei denen keine Kmer-Embeddings generiert wurden
+            # Remove rows without Kmer-Embeddings
             num_before = len(self.merged_input)
             self.merged_input = self.merged_input.dropna(subset=[col for col in embedding_df.columns if col.startswith("kmer_")])
             num_after = len(self.merged_input)
@@ -457,9 +456,9 @@ class DataLoader:
 
         #logger.info(f"✅ Final merged_input shape after k-mers: {self.merged_input.shape}")
         logger.info(self.merged_input.head(4))
-        logger.info(f"🧪 Total features (excluding targets): {len(feature_cols_merged)}")
-        logger.info(f"🎯 Total targets: {num_phenotype_cols - 2}")
-        logger.info(f"🧾 Feature column sample: {feature_cols_merged} ")
+        logger.info(f"Total features (excluding targets): {len(feature_cols_merged)}")
+        logger.info(f"Total targets: {num_phenotype_cols - 2}")
+        logger.info(f"Feature column sample: {feature_cols_merged} ")
 
 
         preprocessed_data = PreprocessedDataDTO(
