e10d57fc75b5ed78207227bc2fbb36b54bd498fd
MiriamBal <miriam.balzer.97@gmail.com>
2025-12-08 15:17:16 +0000

    corrected missing variable import from constants.py

--- workflow/preprocessing.py
+++ workflow/preprocessing.py
@@ -17,6 +17,7 @@ from constants import ORGANISM_COLUMN
 from constants import MODEL_FOLDER
 from constants import FASTA_DIR
 from constants import W2V_MODEL_PATH
+from constants import RETRAIN_W2V
 from constants import W2V
 from config import EXECUTION_MODE, KMERE
 from execution_modes import ExecutionMode
