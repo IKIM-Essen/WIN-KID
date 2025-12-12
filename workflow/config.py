cc8168b8d677174ab524e8a8911fa65ee084709d
MiriamBal <87427584+MiriamBalzer@users.noreply.github.com>
2025-12-12 16:13:08 +0100

    Add FASTA_DIR and RETRAIN_W2V configuration options

--- workflow/config.py
+++ workflow/config.py
@@ -16,3 +16,5 @@ EXECUTION_MODE = ExecutionMode.PREDICT_AND_SAVE
 MIN_SAMPLE_NUMBER = 15
 LOGGING_LEVEL = logging.INFO
 KMERE = True
+FASTA_DIR = ""
+RETRAIN_W2V = False  # Set to True to retrain model
