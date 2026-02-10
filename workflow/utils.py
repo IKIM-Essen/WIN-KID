e841b49e3f7bd6b282d1833b8f8e58c75ae62ce9
MiriamBal <miriam.balzer.97@gmail.com>
2026-02-10 16:02:48 +0000

    adjusted def save_prediction_results in utils.py to handle pandas with different python versions

--- workflow/utils.py
+++ workflow/utils.py
@@ -406,7 +406,9 @@ def save_prediction_results(dataset_input, preprocessed_input, rf_results_input)
     pred_df = pd.DataFrame(pred_dict)
 
     inv_mapping = {v: k for k, v in RESISTANCE_MAPPING.items()}
-    pred_df = pred_df.replace(inv_mapping)
+    for col in pred_df.columns:
+        pred_df[col] = pred_df[col].map(inv_mapping).fillna(pred_df[col])
+    #pred_df = pred_df.replace(inv_mapping)
 
     pred_df[ID_COLUMN] = preprocessed_input.merged_input[ID_COLUMN]
     cols = [ID_COLUMN] + [col for col in pred_df.columns if col != ID_COLUMN]
