a1098c9c1bb6c72a595babe18797442933e6a8ab
MiriamBal <miriam.balzer.97@gmail.com>
2026-02-10 15:44:17 +0000

    unittest error fixed

--- workflow/utils.py
+++ workflow/utils.py
@@ -158,7 +158,6 @@ def evaluate_per_organism(y_pred, y_true, organism_codes, y_score, target_name):
     organism_codes = np.asarray(organism_codes).ravel()
     y_true = np.asarray(y_true).ravel()
     y_pred = np.asarray(y_pred).ravel()
-
     df = pd.DataFrame({"organism": organism_codes, "y_true": y_true, "y_pred": y_pred})
 
     for i in range(y_score.shape[1]):
